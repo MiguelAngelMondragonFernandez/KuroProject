61fd835a9af0e37e8f7a86b99150656611bc7425
MiguelAngelMondragonFernandez <20223tn026@utez.edu.mx>
2025-03-30 18:24:22 -0600

    [FIX] configuracion del proyectp

--- kuroBack/conversaciones/models.py
+++ kuroBack/conversaciones/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Conversacion(models.Model):
-    participantes = models.JSONField()
+    participantes = models.CharField(max_length=100)
 
     def __str__(self):
         return self.usuario
